be3433f4cba06cf4a1d0726564448bfc8c1f4f7d
Roman <romadraguca@gmail.com>
2023-12-30 15:24:17 +0200

    end of project

--- src/Page/Footer/component/Nav/Nav.jsx
+++ src/Page/Footer/component/Nav/Nav.jsx
@@ -3,18 +3,25 @@ import React from "react";
 import "./Nav.scss";
 
 export default function Nav() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Додає плавність прокрутки
+    });
+  };
+
   return (
     <nav className="footer-nav">
-      <Link className="footer-link" to="/dropfile">
+      <Link className="footer-link" to="/dropfile" onClick={scrollToTop}>
         <span className="footer-nav-item">Drop File</span>
       </Link>
-      <Link className="footer-link" to="/mycar">
+      <Link className="footer-link" to="/mycar" onClick={scrollToTop}>
         <span className="footer-nav-item">My Car</span>
       </Link>
-      <Link className="footer-link" to="/personalSet">
+      <Link className="footer-link" to="/personalSet" onClick={scrollToTop}>
         <span className="footer-nav-item">Personal Setting</span>
       </Link>
-      <Link className="footer-link" to="/listoferror">
+      <Link className="footer-link" to="/listoferror" onClick={scrollToTop}>
         <span className="footer-nav-item">List OF Error</span>
       </Link>
     </nav>
