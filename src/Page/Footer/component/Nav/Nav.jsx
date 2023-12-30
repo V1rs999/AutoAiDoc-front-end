e9185d24554888b47651c4c6eae1df8e494ec41d
Roman <romadraguca@gmail.com>
2023-12-30 15:09:06 +0200

    end of project

--- src/Page/Footer/component/Nav/Nav.jsx
+++ src/Page/Footer/component/Nav/Nav.jsx
@@ -9,10 +9,10 @@ export default function Nav() {
         <span className="footer-nav-item">Drop File</span>
       </Link>
       <Link className="footer-link" to="/mycar">
-        <span className="footer-nav-item">My car</span>
+        <span className="footer-nav-item">My Car</span>
       </Link>
-      <Link className="footer-link" to="#">
-        <span className="footer-nav-item">Features</span>
+      <Link className="footer-link" to="/personalSet">
+        <span className="footer-nav-item">Personal Setting</span>
       </Link>
       <Link className="footer-link" to="/listoferror">
         <span className="footer-nav-item">List OF Error</span>
--- src/Page/Header/component/Nav/Nav.jsx
+++ src/Page/Header/component/Nav/Nav.jsx
@@ -7,10 +7,10 @@ export default function Nav() {
         <span className="header-nav-item">Drop File</span>
       </Link>
       <Link className="header-link" to="/mycar">
-        <span className="header-nav-item">My car</span>
+        <span className="header-nav-item">My Car</span>
       </Link>
-      <Link className="header-link" to="#">
-        <span className="header-nav-item">Features</span>
+      <Link className="header-link" to="/personalSet">
+        <span className="header-nav-item">Personal Setting</span>
       </Link>
       <Link className="header-link" to="/listoferror">
         <span className="header-nav-item">List OF Error</span>
